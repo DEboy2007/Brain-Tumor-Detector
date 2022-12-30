957aeed2a51d42f05cb6ed395f70fd431f1037ed
Dhanush Ekollu <dhanush.ekollu@gmail.com>
2022-12-30 03:28:25 -0800

    Trying again to add frontend code

---
+++ Front end code/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import torch
+from PIL import Image
+import numpy as np
+import plotly.express as px
+
+st.title("Brain Tumor Detector")
+st.subheader("Paste the MRI scan image below to detect if there is a tumor or not")
+image = st.file_uploader("Upload MRI scan", type=["jpg"], accept_multiple_files=False)
+
+if image:
+    image = Image.open(image)
+    model = torch.hub.load("ultralytics/yolov5", "custom", path="best.pt", device="cpu")
+    results = model(image, size=640)
+    fig = px.imshow(np.squeeze(results.render()), aspect="equal")
+
+    st.plotly_chart(fig)
