4ae5c83a051d45f0a337248312b739169763d772
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2023-04-11 14:49:42 -0700

    Changed path of weights

--- Front-end-code/app.py
+++ Front-end-code/app.py
@@ -5,13 +5,16 @@ import numpy as np
 import plotly.express as px
 
 st.title("Brain Tumor Detector")
-st.subheader("Paste the MRI scan image below to detect if there is a tumor or not")
+st.subheader(
+    "Paste the MRI scan image below to detect if there is a tumor or not")
 st.text("Github repository: https://github.com/DEboy2007/Brain-Tumor-Detector")
-image = st.file_uploader("Upload MRI scan", type=["jpg", "jpeg", "png", "webp"], accept_multiple_files=False)
+image = st.file_uploader("Upload MRI scan", type=[
+                         "jpg", "jpeg", "png", "webp"], accept_multiple_files=False)
 
 if image:
     image = Image.open(image)
-    model = torch.hub.load("ultralytics/yolov5", "custom", path="best.pt", device="cpu")
+    model = torch.hub.load("ultralytics/yolov5", "custom",
+                           path="Front-end-code/best.pt", device="cpu")
     results = model(image, size=640)
     fig = px.imshow(np.squeeze(results.render()), aspect="equal")
 
